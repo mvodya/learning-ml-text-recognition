1516cd2e8e989622ef04777e46b2f2ce60a6327b
Mark Vodyanitskiy <mvodya@icloud.com>
2024-10-24 17:48:26 +1000

    Scripts for starting jupyter server locally

---
+++ scripts/start.ps1
@@ -0,0 +1,4 @@
+docker run -it -p 8888:8888 --rm --gpus all `
+-v "${PWD}":/home/jovyan/work `
+quay.io/jupyter/tensorflow-notebook:2024-10-23 `
+start-notebook.py --ip='*' --NotebookApp.token='' --NotebookApp.password=''
---
+++ scripts/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker run -it -p 8888:8888 --rm --gpus all \
+-v "${PWD}":/home/jovyan/work \
+quay.io/jupyter/tensorflow-notebook:2024-10-23 \
+start-notebook.py --ip='*' --NotebookApp.token='' --NotebookApp.password=''
